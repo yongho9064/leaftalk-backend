875bfad640aeb4204b3a60991c5ea74a54011670
yongho9064 <dydgmldydgh@naver.com>
2025-12-07 19:37:19 +0900

    first commit

---
+++ src/main/java/com/example/leaftalk/LeafTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.leaftalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeafTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/leaftalk/LeafTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leaftalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
