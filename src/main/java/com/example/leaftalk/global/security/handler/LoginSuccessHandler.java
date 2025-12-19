b74bc470f208e78d318e76d4270b24586554dc9f
yongho9064 <dydgmldydgh@naver.com>
2025-12-19 15:27:53 +0900

    feat(auth): JWT 기반 로그인 필터 및 성공 핸들러 추가
    - 로그인 요청 처리용 LoginFilter 클래스 구현
    - 인증 성공 시 JWT 발급 및 응답 처리하는 LoginSuccessHandler 추가
    - 인증 성공 시 accessToken, refreshToken 생성 및 반환
    - refreshToken 저장을 위해 AuthService 연동

---
+++ src/main/java/com/example/leaftalk/global/security/handler/LoginSuccessHandler.java
@@ -0,0 +1,47 @@
+package com.example.leaftalk.global.security.handler;
+
+import com.example.leaftalk.domain.auth.service.AuthService;
+import com.example.leaftalk.global.security.jwt.JWTUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+@Qualifier("loginSuccessHandler")
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JWTUtil jwtUtil;
+    private final AuthService authService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        // 로그인 성공시 이메일과 역할 추출
+        String email = authentication.getName();
+        String role = authentication.getAuthorities().iterator().next().getAuthority();
+
+        // JWT 생성
+        String accessToken = jwtUtil.createJWT(email, role, true);
+        String refreshToken = jwtUtil.createJWT(email, role, false);
+
+        authService.addRefresh(email, refreshToken);
+
+        // 응답
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        String json = String.format("{\"accessToken\":\"%s\", \"refreshToken\":\"%s\"}", accessToken, refreshToken);
+        response.getWriter().write(json);
+        response.getWriter().flush();
+    }
+}
